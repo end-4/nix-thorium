c60954c5debe3c94756b3d5c44aae26283130769
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-11 08:12:58 +0700

    update sha

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       version = "120.0.6099.235 - 55";
       src = pkgs.fetchurl {
         url = "https://github.com/Alex313031/thorium/releases/download/M120.0.6099.235/Thorium_Browser_120.0.6099.235_x64.AppImage";
-        sha256 = "sha256-dlfClBbwSkQg4stKZdSgNg3EFsWksoI21cxRG5SMrOM=";
+        sha256 = "sha256-HVqC0uk5Ia1xolLvCwDl42VXAUwkikqRasNdLOe8SUs=";
       };
       appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
     in
@@ -37,7 +37,7 @@
       name = "thorium";
       version = "120.0.6099.235 - 5";
       src = pkgs.fetchurl {
-        url = "https://github.com/Alex313031/Thorium-Raspi/releases/download/M120.0.6099.235/Thorium_Browser_120.0.6099.235_arm64.AppImageZZ";
+        url = "https://github.com/Alex313031/Thorium-Raspi/releases/download/M120.0.6099.235/Thorium_Browser_120.0.6099.235_arm64.AppImage";
         sha256 = "";
       };
       appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
